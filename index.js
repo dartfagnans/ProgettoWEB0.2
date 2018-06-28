f8080643a23c238a7a02296fa1aa5a1f722b3fd5
dartfagnans <giuseppe.fagnani@studenti.unicam.it>
2018-06-28 11:41:42 +0200

    add old codes

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const server = express();
+
+const port = 3000;
+
+server.set ('view engine', 'ejs');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static('public'));
+
+server.get('/', function(req, res) {
+    res.render('home');
+})
+
+server.listen(port, function() {
+    console.log('in ascolto sulla porta ' + port);
+})
